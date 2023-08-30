dbfd55663dd91d8ce1b907b17343949cf7a39db0
DESKTOP-6N0LVAE\cmoll <conmolloy@gmail.com>
2023-08-30 22:15:05 +0100

    renaming vector to vector

--- 2022/rust/day1/src/main.rs
+++ 2022/rust/day1/src/main.rs
@@ -9,16 +9,16 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut temp = 0;
-    let mut rslice: Vec<i32> = Vec::new();
+    let mut r_vec: Vec<i32> = Vec::new();
 
     for line in reader.lines() {
         if let Ok(line) = line {
             if line.is_empty() {
-                if temp > 0 && (rslice.len() < 3 || temp > rslice[rslice.len() - 1]) {
-                    rslice.push(temp);
-                    rslice.sort_by_key(|&x| Reverse(x));
-                    if rslice.len() > 3 {
-                        rslice.truncate(3);
+                if temp > 0 && (r_vec.len() < 3 || temp > r_vec[r_vec.len() - 1]) {
+                    r_vec.push(temp);
+                    r_vec.sort_by_key(|&x| Reverse(x));
+                    if r_vec.len() > 3 {
+                        r_vec.truncate(3);
                     }
                 }
                 temp = 0;
